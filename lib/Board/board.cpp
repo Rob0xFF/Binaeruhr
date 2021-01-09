5ab938281d1b0999dd7ebac88403ea4b9561aec5
Rob0xFF <robbehei@me.com>
2021-01-09 08:15:16 +0100

    added bitmask

--- lib/Board/board.cpp
+++ lib/Board/board.cpp
@@ -3,7 +3,7 @@
 void Board::update(void)
 {
 	updateTime();
-	_binaryHours.setOutput(~(((_dt.hour / 10) << 4) | ((_dt.hour % 10) & 0b00001111)));
-	_binaryMinutes.setOutput(~(((_dt.minute / 10) << 4) | ((_dt.minute % 10) & 0b00001111)));
-	_binarySeconds.setOutput(~(((_dt.second / 10) << 4) | ((_dt.second % 10) & 0b00001111)));
+	_binaryHours.setOutput(~((((_dt.hour / 10) << 4) & 0x30) | ((_dt.hour % 10) & 0x0F)));
+	_binaryMinutes.setOutput(~((((_dt.minute / 10) << 4) & 0x70) | ((_dt.minute % 10) & 0x0F)));
+	_binarySeconds.setOutput(~((((_dt.second / 10) << 4) & 0x70) | ((_dt.second % 10) & 0x0F)));
 }
